f1fde622299b22b5ade4f1a3997efb728fdaeed6
Gabriel Oliveira <gabrieloliveira@frwk.com.br>
2022-01-24 13:55:06 -0300

    v2 - Criando loading para requisições http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,13 @@ import { SharedComponentsModule } from './shared/components/shared-components.mo
     SharedComponentsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoadingComponent } from './loading/loading.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    LoadingComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavBarComponent
+    NavBarComponent,
+    LoadingComponent
   ]
 })
 export class SharedComponentsModule { }
